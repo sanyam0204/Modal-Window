e188c3099dd832f981e76aabf70c18298c679bf4
sanyam0204 <101315690+sanyam0204@users.noreply.github.com>
2024-03-17 14:01:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const modal=document.querySelector('.modal');
 const overlay=document.querySelector('.overlay');
 const btnCloseModal=document.querySelector('.close-modal');
 const btnsOpenModal=document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+//console.log(btnsOpenModal);
 
 const fnclose=function (){
-  console.log(`Model Closed`);
+  //console.log(`Model Closed`);
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 }
 
 const fnopen= function()
 {
-  console.log(`Button Clicked`);
+  //console.log(`Button Clicked`);
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 }
