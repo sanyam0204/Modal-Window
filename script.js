c5346e4698ab6b783f7bb10ba7f50416cec2b5be
sanyam0204 <101315690+sanyam0204@users.noreply.github.com>
2024-03-17 13:58:41 +0530

    Commit 1

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+const modal=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btnCloseModal=document.querySelector('.close-modal');
+const btnsOpenModal=document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+const fnclose=function (){
+  console.log(`Model Closed`);
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+const fnopen= function()
+{
+  console.log(`Button Clicked`);
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+for(let i=0; i<btnsOpenModal.length; i++)
+{
+  btnsOpenModal[i].addEventListener('click',fnopen);
+}
+btnCloseModal.addEventListener('click',fnclose);
+overlay.addEventListener('click',fnclose);
+document.addEventListener('keydown', function(e) {
+  if(e.key==='Escape')
+  {
+    if(modal.classList.contains('hidden'))
+    fnopen();
+    else
+    fnclose();
+  }
+});
